a2ee0f1e4a82b36150ef43a229ddcc1f856e5d23
Thomas Wright <thomas.wright@mwrinfosecurity.com>
2014-08-21 17:27:18 +0100

    implement validations on person + test

--- test/models/person_test.rb
+++ test/models/person_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class PersonTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test 'Person requires first_name' do
+    person_requires(:first_name)
+  end
+
+  test 'Person requires last_name' do
+    person_requires(:last_name)
+  end
+
+  def person_requires(field)
+    number_of_people = Person.count
+    test = Person.new
+    test.attributes.each do |key, value|
+      test.attributes[key] = "foo" unless key = field.to_s
+    end
+    test.save
+    assert_equal number_of_people, Person.count
+  end
+
 end
