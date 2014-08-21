94a4e5a9f7fbf9382229b9eb797b8fa399cb9077
Thomas Wright <thomas.wright@mwrinfosecurity.com>
2014-08-21 17:35:26 +0100

    fix person validity tests

--- test/models/person_test.rb
+++ test/models/person_test.rb
@@ -3,20 +3,14 @@ require 'test_helper'
 class PersonTest < ActiveSupport::TestCase
 
   test 'Person requires first_name' do
-    person_requires(:first_name)
+    number_of_people = Person.count
+    Person.create last_name: "bar"
+    assert_equal number_of_people, Person.count
   end
 
   test 'Person requires last_name' do
-    person_requires(:last_name)
-  end
-
-  def person_requires(field)
     number_of_people = Person.count
-    test = Person.new
-    test.attributes.each do |key, value|
-      test.attributes[key] = "foo" unless key = field.to_s
-    end
-    test.save
+    Person.create first_name: "foo"
     assert_equal number_of_people, Person.count
   end
 
