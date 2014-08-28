b4dcb64057728641b42ef69fcca509d46fbd6780
Thomas Wright <thomas.wright@mwrinfosecurity.com>
2014-08-28 16:41:46 +0100

    add phone number creation link to people view

--- test/integration/person_view_test.rb
+++ test/integration/person_view_test.rb
@@ -7,6 +7,13 @@ class PersonViewTest < ActionDispatch::IntegrationTest
   test 'phone number view displays phone number' do
     Person.first.phone_numbers.create(number: 0123)
     visit person_path(Person.first)
-    assert_equal true, page.has_content?(Person.first.phone_numbers.first.number) 
+    assert_equal true, page.has_content?(Person.first.phone_numbers.first.number)
   end
+
+  test 'person view has link to create new phone number' do
+    visit person_path(Person.first)
+    page.click_link('Add phone number')
+    assert_equal true, page.has_content?('New phone_number')
+  end
+
 end
--- test/integration/phone_number_view_test.rb
+++ test/integration/phone_number_view_test.rb
@@ -4,12 +4,9 @@ require "capybara/rails"
 class PhoneNumberViewTest < ActionDispatch::IntegrationTest
   include Capybara::DSL
 
-  setup do
-    #any required setup
-  end
-
   test 'phone number view displays phone number' do
     visit(phone_number_path(PhoneNumber.first))
-    assert_equal true, page.has_content?(PhoneNumber.first.number)
+    assert page.has_content?(PhoneNumber.first.number)
   end
+
 end
