4829da9fd8dc0f08cf96b88d798a22e02b3f8612
Thomas Wright <thomas.wright@mwrinfosecurity.com>
2014-09-01 16:00:14 +0100

    implement required field validations

--- app/models/email_address.rb
+++ app/models/email_address.rb
@@ -1,3 +1,4 @@
 class EmailAddress < ActiveRecord::Base
+  validates :address, :person_id, presence: true
   belongs_to :person
 end
--- test/models/email_address_test.rb
+++ test/models/email_address_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class EmailAddressTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test 'address is required field' do
+    assert_no_difference 'Person.first.email_addresses.count' do
+      Person.first.email_addresses.create(address: nil)
+    end
+  end
+
+  test 'address must be associated with person' do
+    assert_no_difference 'EmailAddress.count' do
+      EmailAddress.create(person_id: nil, address: '1 foo st')
+    end
+  end
+
 end
